c03a1cec1f24ea73ddcccc0367f6e762ab4d4f4c
Ayoub ELorfi <ayouborfi@gmail.com>
2019-11-23 16:14:31 +0100

    initial commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    /**
+     *  @Route("/")
+     */
+    public function homepage(){
+        return new Response('OMG my first page already ! ');
+    }
+
+    /**
+     *  @Route("/news/{name}-{id}")
+     */
+    public function show($name,$id){
+        $comments=[
+            'li content 1 ' ,
+            'li content 2 ' ,
+            'li content 3 '
+        ];
+        return $this->render("article/show.html.twig" ,[
+            'title'=> ucwords(str_replace('-',' ',$name)),
+            'id'=> $id,
+            'comments'=>$comments
+        ]);
+    }
+}
+
+
+?>
